2893133ff9bc33a6c30122bc99583d20eb13fa97
ignaciojeria <ignaciovl.j@gmail.com>
2024-03-31 18:04:02 -0300

    default template server and configuration base files added

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	_ "archetype/app/configuration"
+	"log"
+	"net/http"
+	"os"
+
+	ioc "github.com/Ignaciojeria/einar-ioc"
+)
 
 func main() {
-	fmt.Println("hello from standard template")
+	if err := ioc.LoadDependencies(); err != nil {
+		log.Fatal(err)
+	}
+	port := os.Getenv("Port")
+	log.Println("starting server at port :" + port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal("error starting server : ", err)
+	}
 }
