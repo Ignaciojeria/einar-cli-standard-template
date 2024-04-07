980468b6b081655a870f8f0335be4a187de4234e
ignaciojeria <ignaciovl.j@gmail.com>
2024-04-06 22:46:29 -0300

    gemini prompt fixed

--- app/adapter/out/chat/chat.go
+++ app/adapter/out/chat/chat.go
@@ -11,7 +11,7 @@ type IChat interface {
 	SendMessage(ctx context.Context, domain interface{}) (interface{}, error)
 }
 
-type chat struct {
+type chatStruct struct {
 	model gemini.Gemini1Dot0ProModelWrapper
 }
 
@@ -19,18 +19,22 @@ func init() {
 	ioc.Registry(NewChat, gemini.NewGemini1Dot0ProModelWrapper)
 }
 func NewChat(model gemini.Gemini1Dot0ProModelWrapper) IChat {
-	return chat{
+	return chatStruct{
 		model: model,
 	}
 }
 
-func (s chat) SendMessage(ctx context.Context, domain interface{}) (interface{}, error) {
+func (s chatStruct) SendMessage(ctx context.Context, domain interface{}) (interface{}, error) {
 	// design your custom prompt and chat here using gemini-1.0-pro model
-	return s.model.EphemeralChatExpectJSONResult(ctx, `
-	Write a JSON object in a single line.
+	return s.model.EphemeralChatExpectJSONResult(ctx, `Write a JSON object on a single line with the following properties:
 
-	Here's an example format for your request:
-	{"Message":"Hello World"}`)
+	* A random number between 1 and 100 for the "age" property.
+	* A random string of 10 characters for the "name" property (avoid special characters if possible). 
+	* A random boolean value for the "active" property.
+	* A list of 3 random numbers between 1 and 50 for the "scores" property.
+	
+	Here's a basic example format:
+	{ "age": 52, "name": "JohnDoe", "active": true, "scores": [12, 45, 23] }`)
 }
 
 func Instance() IChat {
